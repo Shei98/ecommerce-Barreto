93853642be0297fc703ad71658797dbdd3e45f51
sheilaba <barretoshei@gmail.com>
2022-09-22 10:02:33 -0300

    arreglé el itemCount

--- budines/src/components/ItemCount/ItemCount.jsx
+++ budines/src/components/ItemCount/ItemCount.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
-const ItemCount = (props) => {
-  const [count, setCount] = React.useState(props.initial);
+// const ItemCount = (props) => {
+//   const [count, setCount] = React.useState(props.initial);
 
-  function handleAdd() {
-    setCount(count + 1);
-  }
-  function handleSubstract() {
+//   function handleAdd() {
+//     setCount(count + 1);
+//   }
+//   function handleSubstract() {
+//     setCount(count - 1);
+//   }
+export const ItemCount = ({ initial, stock, onAdd }) => {
+  const [count, setCount] = useState(initial);
+
+  const decrease = () => {
     setCount(count - 1);
-  }
+  };
+
+  const increase = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div>
-      <button onClick={handleSubstract}>-</button>
+      <button onClick={decrease} disabled={count <= 1}>{" "} - {" "} </button>
       <span>{count} </span>
-      <button onClick={handleAdd}>+</button>
-      <button>Finalizar Compra</button>
+      <button onClick={increase} disabled={count >= stock}>{" "} + {" "} </button>
+      <div className="btnAgregarCarrito">
+      <button disabled={stock <=0} onClick={() => onAdd (count)} >Agregar al carrito</button>{" "}
+      </div>
     </div>
   );
 };
